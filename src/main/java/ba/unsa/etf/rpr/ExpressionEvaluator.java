5139ec15dbdf0112cd2d828a5c98f943d8a4f6ee
azunic3 <azunic3@etf.unsa.ba>
2022-11-14 23:30:39 +0100

    novi testovi

--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -5,116 +5,69 @@ import java.util.Stack;
 /**
  * @author Azra Žunić
  * @version 1.0
- * method ExpressionEvaluator,
+ * method ExpressionEvaluator used for calculating Dijkstr algorithm
+ * code mainly copied from task on c2
+ * objects are inserted and removed according to the first-in first-out (FIFO) principle.
  */
 public class ExpressionEvaluator {
-    private static Stack<Double> val = new Stack<>();
-    private static Stack<Double> op = new Stack<>();
+    private static Stack<Double> vals = new Stack<>();
+    private static Stack<String> ops = new Stack<>();
 
+    /**
+     * parsing string and putting words in an array
+     * @param s
+     * @return s.split
+     */
     public static String[] parsedString(String s){
-        String [] c = s.split(" ");
-
+        String [] c = s.split(" "); //napravi novi element u stacku kad naidje na razmak
         return s.split(" ");
     }
-    /**
-     * a private method for performing given arithmetic operations
-     * @param o an operand to be used in arithmetic operations of type String
-     * @param  a a double value which will be used in an arithmetic operation with operand o
-     * @param b  a double value which will be used in an arithmetic operation with operand o
-     * @return  a double value which represents the result of an arithmetic operation
-     */
-    public static double value(String o, double a, double b){
-        double result = 0;  //has to be intitialised
-//da li se u switch automatski koristi equals?
-        if(o.equals("+")){  //equals!!!
-            result = a + b;
-        }
-        else if(o.equals("-")){
-            result = a - b;
-        }
-        else if(o.equals("*") ){
-            result = a * b;
-        }
-        if(o.equals("/")){
-            result = a / b;
-        }
-
-        return result;
-    }
 
     /**
-     * checks if the expression is in the right format, if the valid operators and operands are used
-     * @param s String to be validated
+     * using foreach loop to go through an array
+     * covering all input cases with if - else as defined in a task
+     * using Java String equals() Method
+     * @param string
+     * @return value of expression sent as a parameter of evaluate method
      */
-
-    public static void validate(String s){
-        //checks if the expression is in the right format (blank spaces between every part of the expression)
-        int expr = 0;
-        int ws = 0;
-        for(int i = 0; i < s.length(); i++){
-
-            if(s.charAt(i) != ' '){  // ne treba equals jer su char?
-                while(s.charAt(i) >= 0 && s.charAt(i) <= 9) {//conditions based on ASCII code
-                    //if we come across a number, we need to count it aw a whole, and not its separate digits
-                    i = i + 1;
+    public static Double evaluate(String string) {
+        String[] niz = (parsedString(string));
+        for (String e : niz) {
+            if (e.equals("(")) ;
+            else if (e.equals("+")) {
+                ops.push(e);
+            } else if (e.equals("-")) {
+                ops.push(e);
+            } else if (e.equals("*")) {
+                ops.push(e);
+            } else if (e.equals("/")) {
+                ops.push(e);
+            } else if (e.equals("sqrt")) {
+                ops.push(e);
+            } else if (e.equals(")")) {
+                String op = ops.pop();
+                Double v = vals.pop();
+                if (e.equals("+")) {
+                    v = vals.pop() + v;
+                } else if (e.equals("-")) {
+                    v = vals.pop() - v;
+                } else if (e.equals("*")) {
+                    v = vals.pop() * v;
+                } else if (e.equals("/")) {
+                    v = vals.pop() / v;
+                } else if (e.equals("sqrt")) {
+                    Math.sqrt(v);
                 }
-                expr = expr + 1;
             }
-            else ws = ws + 1;
-        }
-
-
-        if(ws < expr - 1) {
-            throw new RuntimeException("Invalid expression!");
-        }
-        //checks if the expression has valid operand / operators
-        for(int i = 0; i < s.length(); i++){
-
-            if(!(s.charAt(i) >= 0 && s.charAt(i) <= 9 || s.charAt(i) == '+' || s.charAt(i) == '-' || s.charAt(i) == '*' || s.charAt(i) == '/'))  // ne treba equals jer su char?
-                throw new RuntimeException("Invalid operands and/or operators!");
-        }
-    }
-
-    // public
-
-    /**
-     * a public class for performing arithmetic operations given in a String expression
-     * @param s a String containing the expression to be calculated
-     * @return a double value representing the result of an expression
-     */
-    public static double evaluate(String s) {
-        //parse s
-        String[] exp = parsedString(s);
-
-        //go through exp
-        for (String x : exp) {
-            double dx = 0;
-            //  val = new Stack<>();
-            // Stack<String> op = new Stack<>();
-            if(x.equals(")")) {  // if we come across a right parentheses, it's time to calculate a value and put it in a val stack
-                while (!(op.peek()).equals("(")) { //while the top is not a left parenthesis
-                    String o = String.valueOf(op.pop());
-                    double b = val.pop();  //b comes first due to FIFO principle of a stack
-                    double a = val.pop();
-                    double v = value(o, a, b);
-                    val.push(v);
-                }
-                op.pop(); //a left parenthesis is now removed
-            }
-            else if (!(x.equals(")"))) {  //operands and operators are  pushed in the right stack
-                if (x.equals("+") || x.equals("-") || x.equals("*") || x.equals("/") || x.equals("("))
-                    op.push(Double.valueOf(x));
-                else {
-                    dx = Double.parseDouble(x);
-                    val.push(dx);
-                }
+            else{
+                vals.push(Double.parseDouble(e));
             }
 
         }
 
-        return val.pop(); //the last remaining value in val stack is the required one
-    }
-
+ return vals.pop();
 }
+}
+
 
 
--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaulatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaulatorTest.java
@@ -5,77 +5,63 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 /**
- * contains unit tests for relavant parts of ExpressionEvaluator class
+ * unit tests for relevant methods of ExpressionEvaluator class
  */
- class ExpressionEvaluatorTest {
+class ExpressionEvaluatorTest extends RuntimeException{
+    /**
+     * testing auxiliary method parsedString
+     * it is expected that method will split string sent as a parameter into separated expressions
+     */
     @Test void testparsedString (){
-        String[] expected = new String[] {"(", "2", "+", "2", ")"};
-        String[] result = ExpressionEvaluator.parsedString("( 2 + 2 )");
+        String[] expected = new String[] {"(", "6", "/", "3", ")", "*", "2"};
+        String[] result = ExpressionEvaluator.parsedString("( 6 / 3 ) * 2");
         assertEquals(expected, result);
     }
 
-    @Test void testvalue (){
-        double expected = 4;
-        double result = ExpressionEvaluator.value("+", 2, 2);
-        assertEquals(expected, result);
-
-    }
-    //checking operators +  and * on a simple expression
+    /**
+     * testing all required arithmetic operators on a simple and more complex expressions:
+     */
 
     @Test void testevaluate1 (){
-        double expected = 12;
-        double result = ExpressionEvaluator.evaluate("( ( 2 + 2 ) * 3 )");
-        assertEquals(expected, result);
-
-    }
-
-    //checking operators -  and / on a simple expression
-    @Test void testevaluate2 (){
         double expected = 3;
-        double result = ExpressionEvaluator.evaluate("( ( 20 - 5 ) / 5 )");
+        double result = ExpressionEvaluator.evaluate("( ( 4 / 4 ) + 2 )");
         assertEquals(expected, result);
-
     }
-//da li se podrazumijeva da je validna operacija??? npr, treba li prov. da li je izraz podijeljen sa 0 i sl?
-    //treba li pr validaciju u parsed?
-    //d li treba pr da li se sastoji od okej elemenata??
 
-    //checking all operators on a more complex expression
+    @Test void testevaluate2 (){
+        double expected = 170;
+        double result = ExpressionEvaluator.evaluate("( ( 9 - 4 ) * ( 2 * ( 8 + 9 ) ) )");
+        assertEquals(expected, result);
+    }
+
     @Test void testevaluate3 (){
-        double expected = 6.6;
-        double result = ExpressionEvaluator.evaluate("( ( ( 40 - 5 * 2 ) + 3 ) / 5 )");
+        double expected = -27;
+        double result = ExpressionEvaluator.evaluate("( ( ( 2 / 2 ) + 2 ) - ( 5 * 6 ) )");
         assertEquals(expected, result);
-
     }
 
-    //testing if an expression is valid
-    @Test void testexception (){
-        double expected = 20;
-        double result = ExpressionEvaluator.evaluate("( 1 + 2 * 8 + 3 )");
-        assertThrows(RuntimeException.class, new Executable() {
-            @Override
-            public void execute() throws Throwable {
-                ExpressionEvaluator.validate("2 + 2 & 3");
-            }
-        }, "Invalid operands and/or operators!");
-
+    @Test void testevaluate4 (){
+        double expected = 0;
+        double result = ExpressionEvaluator.evaluate("( ( ( 3 / 2 ) * 3) - ( 9 / 2 ) )");
+        assertEquals(expected, result);
     }
 
-    //testing different combinations with an operand in a denominator
     @Test void testevaluate5 (){
-        double expected = 2.25;
-        double result = ExpressionEvaluator.evaluate("( 9 / ( 2 * 2 ) )");
+        double expected = 2.8;
+        double result = ExpressionEvaluator.evaluate("( 2 * ( 7 / 5 )");
         assertEquals(expected, result);
-        //KAKO PR AKO JE String[] return type
     }
-    //testing different combinations with operands in a numerator and a denominator
-    @Test void testevaluate6 (){
-        double expected = 9;
-        double result = ExpressionEvaluator.evaluate("( ( 9 * 4 ) / ( 2 * 2 ) )");
-        assertEquals(expected, result);
+@Test void testevaluate6(){
+    double result= ExpressionEvaluator.evaluate("{ ( 3 x 2 ) }");
+    assertEquals(RuntimeException.class, result);
+}
 
-    }
+
+
+
+
+
+
 
 }
