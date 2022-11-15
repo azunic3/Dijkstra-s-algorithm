5138bcb24fedd6e59474f4cfbaddfe5a1843dbd9
azunic3 <azunic3@etf.unsa.ba>
2022-11-15 21:20:46 +0100

    main mi se ne pusha na git??

---
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -0,0 +1,32 @@
+package ba.unsa.etf.rpr;
+import java.util.Scanner;
+
+import static ba.unsa.etf.rpr.ExpressionEvaluator.parsedString;
+
+/**
+ * @version 1.0
+ * @author Azra Žunić
+ * implementing auxiliary method parsedString from ExpressionEvaluator
+ **/
+public class App {
+    public static void main(String[] args) throws RuntimeException {
+
+       try{
+            int length = args.length;
+            if(length %2==0) {
+                throw new RuntimeException();
+            }
+            String string = null;
+            int i=0;
+            while(i!=args.length){
+                string+= args[i] + " ";
+                i++;
+            }
+           ExpressionEvaluator.evaluate(string);
+           double result = ExpressionEvaluator.evaluate(string);
+        }
+       catch(RuntimeException e){
+           System.out.println(e);
+        }
+    }
+}
